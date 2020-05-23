e560c063f41085d331191ee418b7e3167e3291e9
Amjad Almuwallad <amjad.almuwallad@gmail.com>
2020-05-23 21:54:52 +0300

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Amjad Almuwallad on 23/05/2020.
+//  Copyright © 2020 Amjad Almuwallad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
